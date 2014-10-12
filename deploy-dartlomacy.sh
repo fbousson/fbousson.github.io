ec76ea6264d47f6ef562d5e827c8a9c5755841e6
Frederick Bousson <fbousson@gmail.com>
2014-10-13 00:55:21 +0200

    yet another publish

---
+++ deploy-dartlomacy.sh
@@ -0,0 +1,6 @@
+cd /Users/fbousson/development/projects/dartlomacy
+/Users/fbousson/development/tools/dart/dart-sdk/bin/pub build --mode=release --output=/Users/fbousson/development/projects/fbousson.github.io/dartlomacy/
+cd /Users/fbousson/development/projects/fbousson.github.io/
+git add --all
+git commit -m "yet another publish"
+git push
